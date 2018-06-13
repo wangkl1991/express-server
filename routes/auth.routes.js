9aa60144c588acc76552d47831aaa08ed29b5525
Wang, Kailun <kaiwang@microstrategy.com>
2018-06-13 11:22:52 -0400

    first commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,16 @@
+var _ = require('lodash');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
+var jwt = require('jsonwebtoken');
+
+
+var router = express.Router();
+
+/**
+*  Login
+*/
+
+
+module.exports = router;
